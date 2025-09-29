9bfc9c5c3c60a99cbdc7304e0aaedf9255300954
Ravikumar <pandu.ravi218@gmail.com>
2025-09-29 22:03:18 +0530

    Hello

--- 01.sh
+++ 01.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 
-# WEEK=$(date | cut -d " " -f1)
-# echo "$WEEK"
-
-# if [ "$WEEK" -ne "Sunday" ]
-# then 
-#     echo "today go to school"
-# else
-#     echo "today holiday"
-# fi
+WEEK=$(date | cut -d " " -f1)
 
 
-
-
-# Get the day of the week (e.g., Sunday, Monday, etc.)
-day=$(date +%A)
-
-if [ "$day" = "Sunday" ]; then
-    echo "It's Sunday, holiday!"
+if [ "$WEEK" = "Sunday" ];
+then 
+    echo "today go to school"
 else
-    echo "I need to go to school."
+    echo "today holiday"
 fi
+
+
+
+
+# # Get the day of the week (e.g., Sunday, Monday, etc.)
+# day=$(date +%A)
+
+# if [ "$day" = "Sunday" ]; then
+#     echo "It's Sunday, holiday!"
+# else
+#     echo "I need to go to school."
+# fi
