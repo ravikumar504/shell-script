30bf0d449c8cbbaf3aeaa5b1b7fb199e89a79d65
Ravikumar <pandu.ravi218@gmail.com>
2025-09-29 22:16:28 +0530

    Hello

--- 01.sh
+++ 01.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-NUMBER=$1
 
-if [ $NUMBER -gt 100 ]
-then
-    echo "given number is gt 100"
-else
-    echo "given number is less than 100"
-fi 
+
+sudo dnf install mysql-server -y 
+
+
+
+
+
+
+
+# NUMBER=$1
+
+# if [ $NUMBER -gt 100 ]
+# then
+#     echo "given number is gt 100"
+# else
+#     echo "given number is less than 100"
+# fi 
 
 
 # WEEK=$(date | cut -d " " -f1)
