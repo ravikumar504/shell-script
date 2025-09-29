4f3f08dfdc5a4acfccc62b930e15ea2d30409efd
Ravikumar <pandu.ravi218@gmail.com>
2025-09-29 22:51:40 +0530

    Hello

--- 01.sh
+++ 01.sh
@@ -6,9 +6,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "you need root access"
+    exit 1
 fi
-
+ 
 dnf install mysql -y
+
+if [ $? -ne 0]
+then 
+    echo "mysql installation failure"
+else    
+    echo "mysql installation success"
+fi
 
 
 
