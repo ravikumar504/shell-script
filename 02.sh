8fdb7b9bc40007eeb5887e1daa7e9d88cef4713d
Ravikumar <pandu.ravi218@gmail.com>
2025-10-02 19:28:12 +0530

    logs

--- 02.sh
+++ 02.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
- SOURCE="/home/ec2-user/"
+SOURCE="/home/ec2-user/"
 FILESDELETE=$(find $SOURCE -name "*.log" -mtime +14)
 # echo "$FILESDELETE"
 
 while read -r file 
 do  
     echo $file
-done < $FILESDELETE
+    rm -rf $file
+done <<< $FILESDELETE
