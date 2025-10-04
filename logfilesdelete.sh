5a075d43136ec1226f65be3fb2ce4f76f377e963
Ravikumar <pandu.ravi218@gmail.com>
2025-10-04 12:21:34 +0530

    backup

--- logfilesdelete.sh
+++ logfilesdelete.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
+# #!/bin/bash
 
-SOURCE="/home/ec2-user/"
-FILESDELETE=$(find $SOURCE -name "*.log" -mtime +14)
-# echo "$FILESDELETE"
+# SOURCE="/home/ec2-user/"
+# FILESDELETE=$(find $SOURCE -name "*.log" -mtime +14)
+# # echo "$FILESDELETE"
 
-while read -r file 
-do  
-    echo $file
-    rm -rf $file
-done <<< $FILESDELETE
+# while read -r file 
+# do  
+#     echo $file
+#     rm -rf $file
+# done <<< $FILESDELETE
+
+
+
+
+
+FILEPATH=/home/ec2-user/test.txt
+
+if [ -e "$FILEPATH" ] && [ -r "$FILEPATH "]
+then 
+    echo "file exists and readable"
+
+else    
+    echo "file not exist"
+fi
+
